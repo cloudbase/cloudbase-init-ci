403b3fc4437957a5b7774f7b58b73ef8c6935335
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-01-05 11:23:53 +0200

    Test the content of the files created by write-files plugin.

--- argus/tests/generic_tests.py
+++ argus/tests/generic_tests.py
@@ -253,6 +253,12 @@ class GenericTests(scenario.BaseArgusScenario):
             self.assertTrue(expected.issubset(set(files)),
                             "The expected set is not subset of {}"
                             .format(files))
+            for file in files:
+                path = os.path.join("C:\\", file)
+                content = self.instance_utils.get_instance_file_content(path)
+                # The content of the cloudconfig files is '42', encoded
+                # in various forms.
+                self.assertEqual('42', content)
 
         self.expectFailure("cloudconfig expects a couple of patches "
                            "to be merged before actually working.",
