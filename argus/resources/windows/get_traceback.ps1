917da4ad4a1e11735e327dc0dbfba0980ab4d015
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-02-17 20:02:06 +0200

    Fix the passing of parameters to function.

--- argus/resources/windows/get_traceback.ps1
+++ argus/resources/windows/get_traceback.ps1
@@ -10,10 +10,10 @@ else
    $programFilesDir = $ENV:ProgramFiles
 }
 
-Select-string -Path $programFilesDir'\Cloudbase Solutions\Cloudbase-Init\log\cloudbase-init.log'
-              -Pattern 'Traceback\s+\(most\s+recent\s+call\s+last\)'
+Select-string -Path $programFilesDir'\Cloudbase Solutions\Cloudbase-Init\log\cloudbase-init.log' `
+              -Pattern 'Traceback\s+\(most\s+recent\s+call\s+last\)' `
               -AllMatches -Context 10
-Select-string -Path $programFilesDir'\Cloudbase Solutions\Cloudbase-Init\log\cloudbase-init-unattend.log'
-              -Pattern 'Traceback\s+\(most\s+recent\s+call\s+last\)'
-              -AllMatches
+Select-string -Path $programFilesDir'\Cloudbase Solutions\Cloudbase-Init\log\cloudbase-init-unattend.log' `
+              -Pattern 'Traceback\s+\(most\s+recent\s+call\s+last\)' `
+              -AllMatches `
               -Context 10
