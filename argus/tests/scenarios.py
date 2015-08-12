213cacab90f74ed226b7f9a2842019909e038de6
Ionut Hulub <ihulub@cloudbasesolutions.com>
2015-08-12 14:58:57 +0300

    bug fixes

--- argus/tests/scenarios.py
+++ argus/tests/scenarios.py
@@ -1,18 +1,8 @@
-import mock
-
 from argus.backends import tempest_backend as tempest_backend
 from argus.introspection.cloud import windows
 from argus.recipes.cloud import windows as windows_recipes
 from argus.tests import base
-from argus.tests.tests import mock_tests
-
-
-class TestScenario(base.BaseScenario):
-    backend_type = mock.MagicMock()
-    recipe_type = mock.MagicMock()
-    introspection_type = mock.MagicMock()
-
-    test_classes = [mock_tests.RandomTest, mock_tests.NamespaceCollisionTest]
+from argus.tests.cloud.windows import test_smoke
 
 
 class TempestScenario(base.BaseScenario):
@@ -20,4 +10,4 @@ class TempestScenario(base.BaseScenario):
     recipe_type = windows_recipes.CloudbaseinitRecipe
     introspection_type = windows.InstanceIntrospection
 
-    test_classes = [mock_tests.RandomTest, mock_tests.NamespaceCollisionTest]
+    test_classes = [test_smoke.TestSmoke]
