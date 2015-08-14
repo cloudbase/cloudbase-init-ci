41ec4d7309d21c2992e47871d46a7da724930c79
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-08-14 15:53:39 +0300

    Move the scenario base class and scenarios mixins (from the backend) into argus.scenarios

--- argus/backends/tempest/cloud.py
+++ argus/backends/tempest/cloud.py
@@ -30,17 +30,6 @@ SUBNET6_CIDR = "::ffff:a00:0/120"
 DNSES6 = ["::ffff:808:808", "::ffff:808:404"]
 
 
-class named(collections.namedtuple("service", "application script_name "
-                                              "host port")):
-
-    @property
-    def stop_link(self):
-        link = "http://{host}:{port}{script_name}/stop_me/"
-        return link.format(host=self.host,
-                           port=self.port,
-                           script_name=self.script_name)
-
-
 class NetworkWindowsBackend(tempest_backend.BaseWindowsTempestBackend):
     """Backend for providing static network configuration.
 
@@ -198,78 +187,3 @@ class RescueWindowsBackend(tempest_backend.BaseWindowsTempestBackend):
         self._servers_client.unrescue_server(self._server['id'])
         waiters.wait_for_server_status(
             self._servers_client, self._server['id'], 'ACTIVE')
-
-
-class BaseServiceMockMixin(object):
-    """Mixin class for mocking metadata services.
-
-    In order to have support for mocked metadata services, set a list
-    of :meth:`named` entries in the class, as such::
-
-        class Test(BaseServiceMockMixin, BaseWindowsTempestBackend):
-            services = [
-                 named(application, script_name, host, port)
-            ]
-
-    These services will be started and will be stopped after
-    :meth:`prepare_instance` finishes.
-    """
-
-    @contextlib.contextmanager
-    def instantiate_mock_services(self):
-        with service_mock.instantiate_services(self.services, self):
-            yield
-
-    def prepare_instance(self):
-        with self.instantiate_mock_services():
-            super(BaseServiceMockMixin, self).prepare_instance()
-
-
-class EC2WindowsScenario(BaseServiceMockMixin, BaseWindowsScenario):
-    """Scenario for testing the EC2 metadata service."""
-
-    services = [
-        named(application=service_mock.EC2MetadataServiceApp,
-              script_name="/2009-04-04/meta-data",
-              host="0.0.0.0",
-              port=2000),
-    ]
-
-
-class CloudstackWindowsScenario(BaseServiceMockMixin,
-                                BaseWindowsScenario):
-    """Scenario for testing the Cloudstack metadata service."""
-
-    services = [
-        named(application=service_mock.CloudstackMetadataServiceApp,
-              script_name="",
-              host="0.0.0.0",
-              port=2001),
-        named(application=service_mock.CloudstackPasswordManagerApp,
-              script_name="",
-              host="0.0.0.0",
-              port=8080),
-    ]
-
-
-class MaasWindowsScenario(BaseServiceMockMixin, BaseWindowsScenario):
-    """Scenario for testing the Maas metadata service."""
-
-    services = [
-        named(application=service_mock.MaasMetadataServiceApp,
-              script_name="/2012-03-01",
-              host="0.0.0.0",
-              port=2002),
-    ]
-
-
-class HTTPKeysWindowsScenario(BaseServiceMockMixin, BaseWindowsScenario):
-
-    """Scenario for testing custom OpenStack http metadata service."""
-
-    services = [
-        named(application=service_mock.HTTPKeysMetadataServiceApp,
-              script_name="/openstack",
-              host="0.0.0.0",
-              port=2003)
-    ]
--- argus/tests/base.py
+++ argus/tests/base.py
@@ -13,82 +13,14 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import types
 import unittest
-
-import six
 
 
 class BaseTestCase(unittest.TestCase):
-    """
-    Extends unittest.TestCase by adding two member variables to instances,
-    backend and introspection.
-    """
+    """Test case parametrized with a backend and an introspection object."""
 
     def __init__(self, backend, introspection, conf, *args, **kwargs):
         super(BaseTestCase, self).__init__(*args, **kwargs)
         self._backend = backend
         self._introspection = introspection
         self._conf = conf
-
-
-class ScenarioMeta(type):
-    """Metaclass for merging test methods from a given list of test cases."""
-
-    def __new__(mcs, name, bases, attrs):
-        cls = super(ScenarioMeta, mcs).__new__(mcs, name, bases, attrs)
-        test_loader = unittest.TestLoader()
-        if not cls.test_classes:
-            return cls
-        for test_class in cls.test_classes:
-            test_names = test_loader.getTestCaseNames(test_class)
-            for test_name in test_names:
-
-                def delegator(self, class_name=test_class,
-                              test_name=test_name):
-                    getattr(class_name(self.backend, self.introspection,
-                                       test_name), test_name)()
-
-                if hasattr(cls, test_name):
-                    test_name = 'test_%s_%s' % (test_class.__name__,
-                                                test_name)
-
-                # Create a new function from the delegator with the
-                # correct name, since tools such as nose test runner,
-                # will use func.func_name, which will be delegator otherwise.
-                code = six.get_function_code(delegator)
-                func_globals = six.get_function_globals(delegator)
-                func_defaults = six.get_function_defaults(delegator)
-                new_func = types.FunctionType(code, func_globals,
-                                              test_name, func_defaults)
-                setattr(cls, test_name, new_func)
-
-        return cls
-
-
-@six.add_metaclass(ScenarioMeta)
-class BaseScenario(unittest.TestCase):
-    """Scenario which sets up an instance and prepares it using a recipe"""
-
-    backend_type = None
-    introspection_type = None
-    recipe_type = None
-    test_classes = None
-
-    backend = None
-    introspection = None
-    recipe = None
-
-    @classmethod
-    def setUpClass(cls):
-        cls.backend = cls.backend_type()
-        cls.backend.setup_instance()
-
-        cls.recipe = cls.recipe_type(cls.backend)
-        cls.recipe.prepare()
-
-        cls.introspection = cls.introspection_type(cls.backend.remote_client)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.backend.cleanup()
