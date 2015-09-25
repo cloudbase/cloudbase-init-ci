0420e4a37a8286a1f3f8ff22fc48b93c7be00866
Ionut Hulub <ihulub@cloudbasesolutions.com>
2015-09-25 15:33:54 +0300

    fixed read image password from conf

--- argus/backends/tempest/cloud.py
+++ argus/backends/tempest/cloud.py
@@ -174,7 +174,7 @@ class RescueWindowsBackend(tempest_backend.BaseWindowsTempestBackend):
     """Instance rescue Windows-based backend."""
 
     def rescue_server(self):
-        admin_pass = self._conf.argus.default_ci_password
+        admin_pass = self._conf.openstack.image_password
         self._servers_client.rescue_server(self._server['id'],
                                            adminPass=admin_pass)
         waiters.wait_for_server_status(
